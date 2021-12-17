4732c858cfb2405d020d587c5962eea1c518b5f2
saintwithataint <arishabh748@gmail.com>
2021-12-17 23:25:50 +0530

    Updated drama data

--- localApi.py
+++ localApi.py
@@ -1,23 +1,27 @@
 import random, json, sys
 
-gdb = json.load(open('data.json'))
+gdb = json.load(open("data.json"))
+
 
 def fillPhrase(phrase, db):
-    phrase = phrase.split(' ')
+    phrase = phrase.split(" ")
     for i in range(len(phrase)):
         word = phrase[i]
-        for j in db['replacers'].keys():
+        for j in db["replacers"].keys():
             if phrase[i].startswith(j):
-                phrase[i] = phrase[i].replace(j, random.choice(db[db['replacers'][j]]))
+                phrase[i] = phrase[i].replace(j, random.choice(db[db["replacers"][j]]))
                 break
-    return ' '.join(phrase)
+    return " ".join(phrase)
+
 
 def getPhrase(db):
-    return random.choice(db['phrases'])
+    return random.choice(db["phrases"])
+
 
 def generateRandomPhrase(community):
     db = gdb[community]
     return fillPhrase(getPhrase(db), db)
+
 
 if __name__ == "__main__":
     print(generateRandomPhrase(sys.argv[1]))
